57dafc299f65c653c8d0a8845448b24547e3d5e5
Benjamin Dolan <benjamin@thinkful.com>
2020-01-31 16:58:40 -0700

    added ben to repo

--- src/components/Part.js
+++ src/components/Part.js
@@ -24,15 +24,15 @@ function Part(props) {
         );
       });
 
-        return (
-            <fieldset className="feature" key={featureHash}>
-            <legend className="feature__name">
-                <h3>{this.props.feature}</h3>
-            </legend>
-            {options}
-            </fieldset>
+      return (
+          <fieldset className="feature" key={featureHash}>
+          <legend className="feature__name">
+              <h3>{this.props.feature}</h3>
+           </legend>
+          {options}
+          </fieldset>
       );
-    }
-});
+  }
+  
 
 export default Part;
